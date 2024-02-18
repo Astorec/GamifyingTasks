e1abca223d3b12258777dc592f723f58f308f3de
astorec <rossa1298@gmail.com>
2024-02-18 10:41:24 +0000

    Additional Changes

--- src/GamifyingTasks.Server/Firebase/DB/DBCore.Tasks.cs
+++ src/GamifyingTasks.Server/Firebase/DB/DBCore.Tasks.cs
@@ -25,30 +25,3 @@ namespace GamifyingTasks.Firebase.DB{
         }
     }
 }
-using GamifyingTasks.Classes;
-using GamifyingTasks.Firebase.DB.Interfaces;
-
-namespace GamifyingTasks.Firebase.DB{
-    public class  DBCoreTasks : ITasks
-    {
-        public Task CreateTask(Tasks task)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task DeleteTask(string uid)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<List<Tasks>> GetTasks()
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task UpdateTask(Task task)
-        {
-            throw new NotImplementedException();
-        }
-    }
-}
